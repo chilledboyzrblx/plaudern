35c9cba382997c3aba66a71cb924b4fb2932b289
Mr.GeschVed <86187817+chilledboyzrblx@users.noreply.github.com>
2021-11-26 17:01:09 +0530

    Add files via upload

--- plaudern_room.js
+++ plaudern_room.js
@@ -1,3 +1,46 @@
-function logout(){
+const firebaseConfig = {
+    apiKey: "AIzaSyC7Ekxi6ZL9hcKzAfadb1YBE5qzbpsiMHI",
+    authDomain: "plaudern-database.firebaseapp.com",
+    databaseURL: "https://plaudern-database-default-rtdb.firebaseio.com",
+    projectId: "plaudern-database",
+    storageBucket: "plaudern-database.appspot.com",
+    messagingSenderId: "448407541233",
+    appId: "1:448407541233:web:96863acc6c74989735e050"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+function logout() {
     window.location = "index.html";
+}
+
+function addRoom() {
+    room_name = document.getElementById("roomNameInput").value;
+
+    firebase.database().ref("/").child(room_name).update({
+        purpose: "adding room name"
+    });
+
+    localStorage.setItem("room_name", room_name);
+    window.location = "plaudern_page.html";
+}
+
+function getData() {
+    firebase.database().ref("/").on('value',
+        function (snapshot) {
+            document.getElementById("output").innerHTML = "";
+            snapshot.forEach(function (childSnapshot) {
+                childKey = childSnapshot.key;
+                Room_names = childKey;
+                //Start code
+                row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>"
+                //End code
+            });
+        });
+}
+getData();
+function redirectToRoomName(){
+    localStorage.setItem("room_name",room_name);
+    
 }
